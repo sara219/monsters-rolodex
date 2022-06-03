25acc0a4c627372a38e9b838096af167d9c05ff4
sara219 <sara97abuawwad@gmail.com>
2022-06-04 00:14:13 +0300

    convert app component to class, build the state and add an array for the monsters and iterater over the array

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import './App.css';
+import { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+     monster: [
+       {
+         name: 'Linda'
+       },
+       {
+         name: 'Frank'
+       }, 
+       {
+         name: 'Jacky'
+       }
+     ]
+  }
+}
+  render() {
+    return (
+    <div className='App'>
+      {/* iterate over the monster array and print the name (obj.name === monster.name) */}
+    {      
+      this.state.monster.map((monster) => {
+        return <h1>{monster.name}</h1>
+      })
+    }
     </div>
-  );
+    )
+  }
 }
 
-export default App;
+export default App
